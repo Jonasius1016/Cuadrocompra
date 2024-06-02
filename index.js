d5b81c477d394e7e7efe05c682a8f54bd16257cb
jonathan aros <jaroshernandez@gmail.com>
2024-06-02 12:45:18 -0400

    cuadro compra terminado

---
+++ index.js
@@ -0,0 +1,24 @@
+precio = 400000
+
+precioSpan = document.querySelector(".precio-inicial");
+precioSpan.innerHTML = precio
+
+let sumar = document.querySelector(".sumar")
+let restar = document.querySelector(".restar")
+
+sumar.addEventListener("click",()=>{
+    let cantidad = document.querySelector(".cantidad")
+    let totalCantidad = parseInt(cantidad.innerHTML)
+    cantidad.innerHTML = ++totalCantidad
+    let totalPagar = document.querySelector(".valortotal")
+    totalPagar.innerHTML = parseInt(cantidad.innerHTML) * precio
+})
+
+restar.addEventListener("click",()=>{
+    let cantidad =document.querySelector(".cantidad")
+    let totalCantidad = parseInt(cantidad.innerHTML)
+    cantidad.innerHTML = Math.max(0, --totalCantidad)
+    let totalPagar = document.querySelector(".valortotal")
+    totalPagar.innerHTML =parseInt(cantidad.innerHTML) * precio
+
+})
